29a474da57b82f510a38c3741e310b7ac8abbbcd
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-23 13:58:18 -0600

    update

--- script.js
+++ script.js
@@ -66,13 +66,14 @@ const displayMovements = function (movements) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
-   <div class="movement_row">
-   <div class="movements_type movements_type--${type}">${i + 1} ${type}</div>
-   <div class="movements_value">${mov}</div>
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1}${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">${mov}</div>
   </div>
   `;
 
-    containerMovements.insertAdjacentHTML('afterbegin', html);
+    containerMovements.insertAdjacentHTML(afterbegin, html);
   });
 };
 displayMovements(account1.movements);
